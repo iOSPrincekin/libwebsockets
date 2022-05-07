0b18c724b9957982d693a206ef7f0e0bf3c3a134
LEE <15267030696@163.com>
2022-05-07 18:04:02 +0800

    1.调整；

--- scripts/build_iOS.sh
+++ scripts/build_iOS.sh
@@ -10,6 +10,6 @@ rm -rf ${build_target}
 cmake ./ -DCMAKE_TOOLCHAIN_FILE=./contrib/iOS.cmake \
   -DUSE_SHARED_MBEDTLS_LIBRARY=1 \
   -DENABLE_PROGRAMS=0 \
-  -Wno-dev && \
+  -Wno-dev -B ${build_target} && \
   make -j
 
